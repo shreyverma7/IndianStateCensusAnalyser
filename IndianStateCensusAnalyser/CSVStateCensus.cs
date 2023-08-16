94ede16e0b2141979281fe9d5b18d068f8b27453
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-16 11:19:05 +0530

    [Shrey] Add : Ability to load information from csv FIle

---
+++ IndianStateCensusAnalyser/CSVStateCensus.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CSVStateCensus
+    {
+        public static int ReadStateCensusData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCencusData>().ToList();
+                    Console.WriteLine("Read Data From CSV");
+                    foreach (var data in records)
+                    {
+                        Console.WriteLine(data.State + "---" + data.Population + "---" + data.AreaInSqKm + "---" + data.DensityPerSqKm);
+                    }
+                    return records.Count() - 1;
+                }
+            }
+        }
+    }
+}
---
+++ IndianStateCensusAnalyser/StateCensusAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class StateCensusAnalyser
+    {
+        public static int ReadStateCensusData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCencusData>().ToList();
+                    Console.WriteLine("Read Data From CSV");
+                    foreach (var data in records)
+                    {
+                        Console.WriteLine(data.State + "---" + data.Population + "---" + data.AreaInSqKm + "---" + data.DensityPerSqKm);
+                    }
+                    return records.Count() - 1;
+                }
+            }
+        }
+    }
+}
