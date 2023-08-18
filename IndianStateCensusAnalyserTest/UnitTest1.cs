d97d37a22aad54e66c4823986ff8bc35acf28980
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-08-18 10:48:13 +0530

    [Shrey] Add : Abiltiy Check exception in State code

--- IndianStateCensusAnalyserTest/UnitTest1.cs
+++ IndianStateCensusAnalyserTest/UnitTest1.cs
@@ -10,6 +10,13 @@ namespace IndianStateCensusAnalyserTest
         public string stateCensusDataFilePath3 = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCensus.csv";
         public string StateCensusDataDelimeterFilePath = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCensusDataDelimeter.csv";
 
+        public string stateCodeDataFilePath = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCode.csv";
+        public string stateCodeDataTxtFilePath = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCode.txt";
+        public string stateCodeDataNotExistFilePath = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCod.csv";
+        public string stateCodeDataHeaderFilePath = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCodeHeader.csv";
+        public string stateCodeDataDelimeterFilePath = @"D:\Bridgelabz Problem statement\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCodeDelimeter.csv";
+
+
 
         [Test]
         public void GivenStateCencusData_WhenAnalysed_RecordsShouldBeMatched()
@@ -63,6 +70,63 @@ namespace IndianStateCensusAnalyserTest
                 StateCensusAnalyser.ReadStateCensusData(StateCensusDataDelimeterFilePath);
             }
             catch (CensusAnalyserException ex)
+            {
+                Assert.AreEqual(ex.Message, "Delimeter Incorrect");
+            }
+        }
+        //---------------------------------------------
+        [Test]
+        public void GivenStateCodeData_WhenAnalysed_RecordsShouldBeMatched()
+        {
+            Assert.AreEqual(StateCodeAnalyser.ReadStateCodeData(stateCodeDataFilePath), CSVStateCode.ReadStateCodeData(stateCodeDataFilePath));
+            Assert.Pass();
+        }
+        [Test]
+        public void GivenStateCodeDataFileIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeAnalyser.ReadStateCodeData(stateCodeDataTxtFilePath);
+            }
+            catch (StateCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "File extension incorrect");
+            }
+
+        }
+         [Test]
+        public void GivenStateCodeDataFileNotExists_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeAnalyser.ReadStateCodeData(stateCodeDataNotExistFilePath);
+            }
+            catch (StateCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "File not exists");
+            }
+
+        }
+        [Test]
+        public void GivenStateCodeHeaderIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeAnalyser.ReadStateCodeData(stateCodeDataHeaderFilePath);
+            }
+            catch (StateCodeException ex)
+            {
+                Assert.AreEqual(ex.Message, "Header Incorrect");
+            }
+        }
+        [Test]
+        public void GivenStateCodeDelimeterIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeAnalyser.ReadStateCodeData(stateCodeDataDelimeterFilePath);
+            }
+            catch (StateCodeException ex)
             {
                 Assert.AreEqual(ex.Message, "Delimeter Incorrect");
             }
